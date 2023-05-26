6669552c80ce87a9d44ee3e77141a27d8a939254
thommann <thomasrolf.mannhart@uzh.ch>
2023-05-26 14:43:49 +0200

    profiling

--- playground.py
+++ playground.py
@@ -1,66 +1,18 @@
-import json
-import os
-import time
+from profiling import query_results, query_profiling
 
-import duckdb
+original_query = """
+SELECT SUM(column0) AS result FROM original;
+"""
 
-config = \
-    "PRAGMA enable_profiling='json';" \
-    "PRAGMA profile_output='data/profiles/webb.json';" \
-    "EXPLAIN ANALYZE "
+kronecker_query = """
+SELECT
+(SELECT SUM(column0) FROM matrix_a) * 
+(SELECT SUM(column0) FROM matrix_b) AS result;
+"""
 
-original_query = \
-    "SELECT SUM(column0) AS result FROM original;"
+database = "data/databases/webb.db"
+original_result, kronecker_result = query_results(original_query, kronecker_query, database)
+print(f"Error: {abs((original_result - kronecker_result) / original_result):.2%}")
 
-kronecker_query = \
-    "SELECT " \
-    "(SELECT SUM(column0) FROM matrix_a) * " \
-    "(SELECT SUM(column0) FROM matrix_b) AS result;"
-
-results = []
-
-original_timings = []
-kronecker_timings = []
-
-queries = [("Original", original_query, original_timings),
-           ("Kronecker", kronecker_query, kronecker_timings)]
-
-# con = duckdb.connect(database='data/databases/webb.db')
-# for name, query, _ in queries:
-#     print()
-#     print(name)
-#     con.sql(query).show()
-#     res = con.sql(query).fetchall()
-#     results.append(res[0][0])
-# con.close()
-#
-# print(f"Error:\t{abs((results[0] - results[1]) / results[0]):.2%}")
-
-
-for _ in range(1):
-    for _, query, timings in queries:
-        con = duckdb.connect(database='data/databases/webb.db')
-        con.execute(config + query)
-        con.close()
-
-        # sleep for 1 second to make sure the file is written
-        time.sleep(1)
-
-        with open('data/profiles/webb.json', 'r') as f:
-            profile = json.load(f)
-            timings.append(profile["timing"])
-
-            # Cross product children
-            cp_children = profile["children"][0]["children"][0]["children"]
-            print(json.dumps(cp_children, indent=4))
-
-        # Delete file
-        os.remove('data/profiles/webb.json')
-
-print("Original timings:", original_timings)
-print("Kronecker timings:", kronecker_timings)
-
-average_original_time = sum(original_timings) / len(original_timings)
-average_kronecker_time = sum(kronecker_timings) / len(kronecker_timings)
-
-print(f"Speedup:\t{average_original_time / average_kronecker_time:.2f}x")
+original_time, kronecker_time = query_profiling(original_query, kronecker_query, database)
+print(f"Speedup: {original_time / kronecker_time:.1f}x")
---
+++ profiling.py
@@ -0,0 +1,58 @@
+import json
+import time
+
+import duckdb
+
+config = """
+PRAGMA enable_profiling='json';
+PRAGMA profile_output='.temp';
+PRAGMA threads=4;
+"""
+
+
+def query_results(original_query: str, kronecker_query: str, database: str) -> tuple[float, float]:
+    con = duckdb.connect(database=database)
+    con.execute(config)
+
+    original_result = con.sql(original_query).fetchall()[0][0]
+    kronecker_result = con.sql(kronecker_query).fetchall()[0][0]
+
+    con.close()
+
+    return original_result, kronecker_result
+
+
+def query_profiling(original_query: str,
+                    kronecker_query: str,
+                    database: str,
+                    runs: int = 10,
+                    epochs: int = 1) -> tuple[float, float]:
+    original_timings = []
+    kronecker_timings = []
+
+    queries = [(original_query, original_timings),
+               (kronecker_query, kronecker_timings)]
+
+    con = duckdb.connect(database=database)
+    con.execute(config)
+
+    for _ in range(epochs):
+        for query, timings in queries:
+            for _ in range(runs):
+                res = con.sql(query).explain('analyze')
+
+                res_json = json.loads(res)
+                timings.append(res_json["timing"])
+
+                # sleep for 1 second to make sure the file is written
+                time.sleep(1)
+
+    con.close()
+
+    original_timings_truncated = original_timings[1:]
+    kronecker_timings_truncated = kronecker_timings[1:]
+
+    average_original_time = sum(original_timings_truncated) / len(original_timings_truncated)
+    average_kronecker_time = sum(kronecker_timings_truncated) / len(kronecker_timings_truncated)
+
+    return average_original_time, average_kronecker_time
