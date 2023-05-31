f8efccfdd7b99e12418bc8bf0453299eef2e1fc2
thommann <thomasrolf.mannhart@uzh.ch>
2023-05-31 18:37:01 +0200

    bash scripts

---
+++ hello_world.py
@@ -0,0 +1,15 @@
+import sys
+
+
+def main():
+    print("Hello World!")
+    # Print python version
+    print(sys.version)
+    # Print python version info
+    print(sys.version_info)
+    # Print python executable path
+    print(sys.executable)
+
+
+if __name__ == "__main__":
+    main()
