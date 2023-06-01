2ef7c2b571cde4a1ddfefe2de41560c78cb49f9a
thommann <thomasrolf.mannhart@uzh.ch>
2023-06-01 16:47:25 +0200

    opt smaller model

--- opt.py
+++ opt.py
@@ -1,8 +1,11 @@
 from transformers import OPTForCausalLM
 
-model = OPTForCausalLM.from_pretrained("facebook/opt-66b")
+model = OPTForCausalLM.from_pretrained("facebook/opt-30b")
 weights = model.state_dict()
 
 # write the token embedding weights to a csv file using numpy
 key = 'model.decoder.embed_tokens.weight'
+
+weight_matrix = weights[key].numpy()
+print(weight_matrix.shape)
 # np.savetxt(f"{key}.csv", weights[key].numpy(), delimiter=",")
