51634f712ccfacf7a574cab9c3753dc5e974d6bb
thommann <thomasrolf.mannhart@uzh.ch>
2023-06-01 17:12:18 +0200

    update hello world

--- hello_world.py
+++ hello_world.py
@@ -1,14 +1,18 @@
 import sys
+from time import sleep
 
 
 def main():
-    print("Hello World!")
+    print("Hello World!", flush=True)
+    sleep(10)
     # Print python version
-    print(sys.version)
+    print(sys.version, flush=True)
+    sleep(10)
     # Print python version info
-    print(sys.version_info)
+    print(sys.version_info, flush=True)
+    sleep(10)
     # Print python executable path
-    print(sys.executable)
+    print(sys.executable, flush=True)
 
 
 if __name__ == "__main__":
