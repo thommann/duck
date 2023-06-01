668bd086e5bc0197b19620b89e5f7774e09b921a
thommann <thomasrolf.mannhart@uzh.ch>
2023-06-01 17:02:15 +0200

    run nicely

--- scripts/run_python_script.sh
+++ scripts/run_python_script.sh
@@ -1,6 +1,32 @@
 #!/bin/bash
 
+# Flag indicating if nice should be used
+USE_NICE=0
+
+# Parse command line options
+while getopts "n" opt; do
+  case ${opt} in
+    n)
+      USE_NICE=1
+      ;;
+    \?)
+      echo "Invalid option: -$OPTARG" 1>&2
+      exit 1
+      ;;
+  esac
+done
+
+# Remove parsed options and args from $@ list
+shift $((OPTIND -1))
+
 source venv/bin/activate
 name=${1%.py}
-nohup python3 "$1" >& out/"$name".log &
+
+# Your command
+if [ "$USE_NICE" -eq 1 ]; then
+    nohup nice python3 "$1" >& out/"$name".log &
+else
+    nohup python3 "$1" >& out/"$name".log &
+fi
+
 echo $! > out/"$name".pid
