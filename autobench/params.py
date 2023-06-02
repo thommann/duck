369f2109690f2000cfd17218728b047dc25dd52a
thommann <thomasrolf.mannhart@uzh.ch>
2023-06-02 15:59:05 +0200

    useful plots

--- autobench/params.py
+++ autobench/params.py
@@ -2,7 +2,7 @@ rows_base_2 = [2 ** x for x in range(10, 27)]
 rows_base_10 = [10 ** x for x in range(3, 9)]
 # concatenate and sort
 rows = sorted(rows_base_2 + rows_base_10)
-cols = [2]
+cols = [2 ** x for x in range(1, 5)]
 
 max_k = 10
 k = 2
--- autobench/plot_results.py
+++ autobench/plot_results.py
@@ -1,7 +1,9 @@
 import matplotlib.pyplot as plt
 import numpy as np
 
-from autobench.params import rows
+from autobench.params import rows_base_2, cols
+
+rows = rows_base_2
 
 errors_sum = np.loadtxt('data/results/sum_errors.csv', delimiter=',')
 speedups_sum = np.loadtxt('data/results/sum_speedups.csv', delimiter=',')
@@ -9,35 +11,19 @@ errors_sumproduct = np.loadtxt('data/results/sumproduct_errors.csv', delimiter='
 speedups_sumproduct = np.loadtxt('data/results/sumproduct_speedups.csv', delimiter=',')
 
 
-def plot(x: list, y: np.ndarray, title: str, xlabel: str, ylabel: str, filename: str) -> None:
+def plot(y: np.ndarray, title: str, ylabel: str, filename: str) -> None:
     plt.figure()
     plt.title(title)
-    plt.xlabel(xlabel)
+    plt.xlabel('rows')
     plt.ylabel(ylabel)
     plt.xscale('log')
-    plt.plot(x, y)
+    for i, col in enumerate(cols):
+        plt.plot(rows, y[:, i], label=f"{col} cols")
+    plt.legend()
     plt.savefig('data/plots/' + filename + '.png')
 
 
-# plot sum error over rows
-plot(rows, errors_sum.mean(axis=1), 'Sum error over rows', 'rows', 'error', 'sum_error_over_rows')
-
-# plot sumproduct error over rows
-plot(rows, errors_sumproduct.mean(axis=1), 'Sum-product error over rows', 'rows', 'error', 'sumproduct_error_over_rows')
-
-# plot sum speedup over rows
-plot(rows, speedups_sum.mean(axis=1), 'Sum speedup over rows', 'rows', 'speedup', 'sum_speedup_over_rows')
-
-# plot sumproduct speedup over rows
-plot(rows, speedups_sumproduct.mean(axis=1), 'Sum-product speedup over rows', 'rows', 'speedup',
-     'sumproduct_speedup_over_rows')
-
-# plot sum error over rows for min and max cols
-plot(rows, errors_sum[:, 0], 'Sum error over rows for min cols', 'rows', 'error', 'sum_error_over_rows_min_cols')
-plot(rows, errors_sum[:, -1], 'Sum error over rows for max cols', 'rows', 'error', 'sum_error_over_rows_max_cols')
-
-# plot sumproduct error over rows for min and max cols
-plot(rows, errors_sumproduct[:, 0], 'Sum-product error over rows for min cols', 'rows', 'error',
-     'sumproduct_error_over_rows_min_cols')
-plot(rows, errors_sumproduct[:, -1], 'Sum-product error over rows for max cols', 'rows', 'error',
-     'sumproduct_error_over_rows_max_cols')
+# plot sum error
+plot(errors_sum, 'Sum Error', 'Error', 'sum_error')
+# plot sum speedup
+plot(speedups_sum, 'Sum Speedup', 'Speedup', 'sum_speedup')
