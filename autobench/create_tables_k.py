c179645997d8899c507d267ac92f6fa32eab9129
thommann <thomasrolf.mannhart@uzh.ch>
2023-06-02 13:54:58 +0200

    create tables for rank > 1

---
+++ autobench/create_tables_k.py
@@ -0,0 +1,11 @@
+from autobench.params import image, name, rows_base_10
+from src.image_to_db import image_to_db
+
+k = 2
+
+col = 4
+rows = rows_base_10
+
+for row in rows:
+    print(f"rows: {row:,}", flush=True)
+    image_to_db(image, (row, col), name, k=k)
