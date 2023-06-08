9ea9971eacf3bb9e2c11fb493cf2f0a5e85e5f63
thommann <thomasrolf.mannhart@uzh.ch>
2023-06-08 14:18:20 +0200

    query trees

--- execution_plans.py
+++ execution_plans.py
@@ -3,11 +3,13 @@ import duckdb
 
 from queries import queries
 
-col_indices = [0, 1]
+col_indices = [0]
 nr_cols = 2
 nr_rows = 1000000
-rank_k = 2
+rank_k = 1
 max_rank = 10
+individual = False
+output = "txt"  # "txt" or "json"
 
 original, kronecker = queries(col_indices, nr_cols, rank_k, max_rank)
 
@@ -15,13 +17,14 @@ print(original)
 print()
 print(kronecker)
 
-plan_original = f"data/plans/original_{nr_rows}x{nr_cols}_rank{rank_k}_cols{len(col_indices)}.txt"
-plan_kronecker = f"data/plans/kronecker_{nr_rows}x{nr_cols}_rank{rank_k}_cols{len(col_indices)}.txt"
-config = """
-PRAGMA enable_profiling='QUERY_TREE';
+suffix = "_sc" if individual else ""
+plan_original = f"data/plans/original_{nr_rows}x{nr_cols}_rank{rank_k}_cols{len(col_indices)}{suffix}.{output}"
+plan_kronecker = f"data/plans/kronecker_{nr_rows}x{nr_cols}_rank{rank_k}_cols{len(col_indices)}{suffix}.{output}"
+config = f"""
+PRAGMA enable_profiling='{'QUERY_TREE' if output == 'txt' else 'JSON'}';
 PRAGMA threads=48;
 """
-db = f"data/databases/webb_{nr_rows}x{nr_cols}_rank_{max_rank}.db"
+db = f"data/databases/webb_{nr_rows}x{nr_cols}{suffix}_rank_{max_rank}.db"
 
 con = duckdb.connect(db)
 con.execute(config)
--- queries.py
+++ queries.py
@@ -1,9 +1,9 @@
 import itertools
 
 
-def queries(col_indices: list[int], nr_cols: int, rank_k: int, max_rank: int):
+def queries(col_indices: list[int], nr_cols: int, rank_k: int, max_rank: int, individual: bool = False):
     col_format_a = "03d" if nr_cols * max_rank > 100 else "02d" if nr_cols * max_rank > 10 else "01d"
-    col_format_b = "03d" if max_rank > 100 else "02d" if max_rank > 10 else "01d"
+    col_format_b = col_format_a if individual else "03d" if max_rank > 100 else "02d" if max_rank > 10 else "01d"
     col_format_c = "03d" if nr_cols > 100 else "02d" if nr_cols > 10 else "01d"
 
     original_query = f"SELECT SUM({' * '.join([f'column{i:{col_format_c}}' for i in col_indices])}) FROM C;"
@@ -11,11 +11,28 @@ def queries(col_indices: list[int], nr_cols: int, rank_k: int, max_rank: int):
     if len(col_indices) == 1:
         # SUM
         col_idx = col_indices[0]
-        kronecker_query = f"SELECT {' + '.join([f'(SELECT SUM(column{r * nr_cols + col_idx:{col_format_a}}) FROM A) * (SELECT SUM(column{r:{col_format_b}}) FROM B)' for r in range(rank_k)])};"
+        terms = []
+        for r in range(rank_k):
+            col_idx_a = r * nr_cols + col_idx
+            col_idx_b = r * nr_cols + col_idx if individual else r
+            terms.append(f"(SELECT SUM(column{col_idx_a:{col_format_a}}) FROM A) * "
+                         f"(SELECT SUM(column{col_idx_b:{col_format_b}}) FROM B)")
+        kronecker_query = f"SELECT {' + '.join(terms)};"
 
     else:
         # SUM product
-        terms = itertools.product(*[itertools.product([idx], range(rank_k)) for idx in col_indices])
-        kronecker_query = f"""SELECT {' + '.join([f'(SELECT SUM({" * ".join([f"column{r * nr_cols + col_idx:{col_format_a}}" for col_idx, r in term])}) FROM A) * (SELECT SUM({" * ".join([f"column{r:{col_format_b}}" for _, r in term])}) FROM B)' for term in terms])};"""
+        combinations = itertools.product(*[itertools.product([idx], range(rank_k)) for idx in col_indices])
+        terms = []
+        for combination in combinations:
+            products_a = []
+            products_b = []
+            for col_idx, r in combination:
+                col_idx_a = r * nr_cols + col_idx
+                col_idx_b = r * nr_cols + col_idx if individual else r
+                products_a.append(f"column{col_idx_a:{col_format_a}}")
+                products_b.append(f"column{col_idx_b:{col_format_b}}")
+            terms.append(f"(SELECT SUM({' * '.join(products_a)}) FROM A) * "
+                         f"(SELECT SUM({' * '.join(products_b)}) FROM B)")
+        kronecker_query = f"SELECT {' + '.join(terms)};"
 
     return original_query, kronecker_query
