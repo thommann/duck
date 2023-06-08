4fe01ca853efb02d2770bb0f78911b427746ad80
thommann <thomasrolf.mannhart@uzh.ch>
2023-06-08 14:32:00 +0200

    opt weights

--- opt/opt.py
+++ opt/opt.py
@@ -1,8 +1,17 @@
+import numpy as np
 from transformers import OPTForCausalLM
 
 model = OPTForCausalLM.from_pretrained("facebook/opt-30b")
 weights = model.state_dict()
 
+max_i = 0
+i = 0
 for key in weights:
+    i += 1
     print(key, flush=True)
-    print(weights[key].shape, flush=True)
+    value = weights[key]
+    shape = value.shape
+    print(shape, flush=True)
+    np.savetxt(f"data/opt-30b/{key}{shape[0]}x{shape[1]}", np.array(weights[key]))
+    if i > max_i:
+        break
