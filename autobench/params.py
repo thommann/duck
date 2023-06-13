6bc8db16fa1ac05bb3ad253aca027fff9315cd74
thommann <thomasrolf.mannhart@uzh.ch>
2023-06-13 10:15:43 +0200

    timestamp

--- autobench/params.py
+++ autobench/params.py
@@ -5,15 +5,15 @@ rows = sorted(rows_base_2 + rows_base_10)
 cols = [2 ** x for x in range(1, 5)]
 
 max_k = 10
-ks = [1, 2, 3]
+ks = [3, 2, 1]
 
-col_decompositions = ['cc', 'sc', 'nc']
+col_decompositions = ['cc', 'sc', 'nc']  # options: cc, sc, nc
 
-permutations = 2
-factors = [1, 2, 3]
+permutations = 10
+factors = [3, 2, 1]
 
 epochs = 2
-runs = 2
+runs = 1
 
 image = 'data/images/webb.png'
 name = 'webb'
