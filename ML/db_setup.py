c2e7a2c65ca64cedcc285f3f938136f5320b6422
thommann <thomasrolf.mannhart@uzh.ch>
2023-06-14 13:17:18 +0200

    KroneML

--- ML/db_setup.py
+++ ML/db_setup.py
@@ -2,6 +2,11 @@ import duckdb
 import torch
 
 matrices = ['fc1.weight_4x100', 'fc1.bias_100x1', 'fc2.weight_100x50', 'fc2.bias_50x1', 'fc3.weight_50x3', 'fc3.bias_3x1']
+kron_matrices = ['fc1.weight_4x100_a', 'fc1.weight_4x100_b', 'fc1.bias_100x1_a', 'fc1.bias_100x1_b',
+                    'fc2.weight_100x50_a', 'fc2.weight_100x50_b', 'fc2.bias_50x1_a', 'fc2.bias_50x1_b',
+                    'fc3.weight_50x3_a', 'fc3.weight_50x3_b', 'fc3.bias_3x1_a', 'fc3.bias_3x1_b']
+
+matrices += kron_matrices
 
 con = duckdb.connect('lm.db', read_only=False)
 for matrix in matrices:
