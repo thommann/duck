448a7ebddd5627632ab1a73cbbc276bf35f34091
thommann <thomasrolf.mannhart@uzh.ch>
2023-06-14 10:05:48 +0200

    inference views are too memory intensive

--- ML/inference.py
+++ ML/inference.py
@@ -79,35 +79,34 @@ relu = "SELECT h.row_id, CASE WHEN h.value < 0 THEN 0 ELSE h.value END AS value 
 
 softmax = "WITH Exponents AS (" \
           "SELECT h.row_id, EXP(h.value) AS value " \
-          "FROM h3 h), " \
+          "FROM h3 h),\n" \
           "Denominator AS (" \
           "SELECT SUM(value) AS value " \
-          "FROM Exponents) " \
+          "FROM Exponents)\n" \
           "SELECT e.row_id, e.value / d.value AS value " \
           "FROM Exponents e, Denominator d"
 
 # Output
 # FC1
 a1 = "CREATE OR REPLACE TABLE a1 AS (" + w1 + ")"
-print(a1)
 con.execute(a1)
-h1 = "CREATE OR REPLACE TABLE h1 AS " + bias + "FROM a1 a, fc1_bias_100x1 b WHERE a.row_id = b.row_id"
+h1 = "CREATE OR REPLACE TABLE h1 AS (" + bias + "FROM a1 a, fc1_bias_100x1 b WHERE a.row_id = b.row_id)"
 con.execute(h1)
-z1 = "CREATE OR REPLACE TABLE z1 AS " + relu + "FROM h1 h"
+z1 = "CREATE OR REPLACE TABLE z1 AS (" + relu + "FROM h1 h)"
 con.execute(z1)
 
 # FC2
 a2 = "CREATE OR REPLACE TABLE a2 AS (" + w2 + ")"
 con.execute(a2)
-h2 = "CREATE OR REPLACE TABLE h2 AS " + bias + "FROM a2 a, fc2_bias_50x1 b WHERE a.row_id = b.row_id"
+h2 = "CREATE OR REPLACE TABLE h2 AS (" + bias + "FROM a2 a, fc2_bias_50x1 b WHERE a.row_id = b.row_id)"
 con.execute(h2)
-z2 = "CREATE OR REPLACE TABLE z2 AS " + relu + "FROM h2 h"
+z2 = "CREATE OR REPLACE TABLE z2 AS (" + relu + "FROM h2 h)"
 con.execute(z2)
 
 # FC3
 a3 = "CREATE OR REPLACE TABLE a3 AS (" + w3 + ")"
 con.execute(a3)
-h3 = "CREATE OR REPLACE TABLE h3 AS " + bias + "FROM a3 a, fc3_bias_3x1 b WHERE a.row_id = b.row_id"
+h3 = "CREATE OR REPLACE TABLE h3 AS (" + bias + "FROM a3 a, fc3_bias_3x1 b WHERE a.row_id = b.row_id)"
 con.execute(h3)
 output = "CREATE OR REPLACE TABLE output AS (" + softmax + ")"
 con.execute(output)
